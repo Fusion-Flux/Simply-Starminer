01fda0a9743168c75aa226c1084664b66ac66785
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2022-05-17 16:50:46 -0500

    inital commit

--- src/main/java/com/fusionflux/starminer/blockentites/StarCore.java
+++ src/main/java/com/fusionflux/starminer/blockentites/StarCore.java
@@ -6,6 +6,16 @@ import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.stat.Stats;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +29,23 @@ public class StarCore extends BlockWithEntity {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new StarCoreEntity(pos,state);
+    }
+
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) {
+            return ActionResult.SUCCESS;
+        } else {
+            player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+            player.incrementStat(Stats.INTERACT_WITH_ANVIL);
+            return ActionResult.CONSUME;
+        }
+    }
+
+    @Nullable
+    public NamedScreenHandlerFactory createScreenHandlerFactory(BlockState state, World world, BlockPos pos) {
+        return new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> {
+            return new AnvilScreenHandler(syncId, inventory, ScreenHandlerContext.create(world, pos));
+        }, new TranslatableText("Star Core"));
     }
 
     @Override
--- src/main/java/com/fusionflux/starminer/blockentites/StarCoreEntity.java
+++ src/main/java/com/fusionflux/starminer/blockentites/StarCoreEntity.java
@@ -9,13 +9,18 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -26,74 +31,81 @@ public class StarCoreEntity extends BlockEntity {
         super(StarMinerRefabricated.STAR_CORE_ENTITY, pos, state);
     }
 
+    int radius = 0;
+
     public static void tick(World world, BlockPos pos, BlockState state, StarCoreEntity blockEntity) {
-        Box innerBox =(new Box(new BlockPos(pos.getX(),pos.getY(),pos.getZ()))).expand(20);
+        Box innerBox = (new Box(new BlockPos(pos.getX(), pos.getY(), pos.getZ()))).expand(blockEntity.radius);
 
         List<Entity> list = world.getEntitiesByClass(Entity.class, innerBox, e -> true);
 
         for (Entity entity : list) {
-            boolean dontrotate = false;
-            if(entity.hasVehicle()){
-                dontrotate = true;
-            }
+            if (entity != null) {
+                boolean dontrotate = entity.hasVehicle();
 
-            if(entity instanceof PlayerEntity)
-                if(((PlayerEntity) entity).getAbilities().flying)
-                    dontrotate = true;
-            if(!dontrotate) {
-                //listb.remove(entity);
-                ((EntityAttachments) entity).setRemainingGravity(2);
-                if (((EntityAttachments) entity).getGravityTimer() == 0) {
-                double offsetX = Math.abs(innerBox.getCenter().getX() - entity.getPos().getX());
-                double offsetY = Math.abs(innerBox.getCenter().getY() - entity.getPos().getY());
-                double offsetZ = Math.abs(innerBox.getCenter().getZ() - entity.getPos().getZ());
-                if (offsetY > offsetX+2 && offsetY > offsetZ+2) {
-                    if (entity.getPos().getY() > pos.getY()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.DOWN)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.DOWN);
-                    }
-                    if (entity.getPos().getY() < pos.getY()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.UP)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.UP);
+                if (entity instanceof PlayerEntity)
+                    if (((PlayerEntity) entity).getAbilities().flying)
+                        dontrotate = true;
+                if (!dontrotate) {
+                    //listb.remove(entity);
+                    ((EntityAttachments) entity).setRemainingGravity(2);
+                    if (((EntityAttachments) entity).getGravityTimer() == 0) {
+                        double offsetX = Math.abs(innerBox.getCenter().getX() - entity.getPos().getX());
+                        double offsetY = Math.abs(innerBox.getCenter().getY() - entity.getPos().getY());
+                        double offsetZ = Math.abs(innerBox.getCenter().getZ() - entity.getPos().getZ());
+                        if (offsetY > offsetX + 2 && offsetY > offsetZ + 2) {
+                            if (entity.getPos().getY() > pos.getY()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.DOWN)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.DOWN);
+                            }
+                            if (entity.getPos().getY() < pos.getY()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.UP)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.UP);
+                            }
+                        }
+                        if (offsetZ > offsetY + 2 && offsetZ > offsetX + 2) {
+                            if (entity.getPos().getZ() > pos.getZ()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.NORTH)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.NORTH);
+                            }
+                            if (entity.getPos().getZ() < pos.getZ()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.SOUTH)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.SOUTH);
+                            }
+                        }
+
+                        if (offsetX > offsetY + 2 && offsetX > offsetZ + 2) {
+                            if (entity.getPos().getX() > pos.getX()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.WEST)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.WEST);
+                            }
+                            if (entity.getPos().getX() < pos.getX()) {
+                                if (GravityChangerAPI.getGravityDirection(entity) != Direction.EAST)
+                                    ((EntityAttachments) entity).setGravityTimer(5);
+                                GravityChangerAPI.setGravityDirection(entity, Direction.EAST);
+                            }
+                        }
                     }
                 }
-                if (offsetZ > offsetY+2 && offsetZ > offsetX+2) {
-                    if (entity.getPos().getZ() > pos.getZ()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.NORTH)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.NORTH);
-                    }
-                    if (entity.getPos().getZ() < pos.getZ()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.SOUTH)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.SOUTH);
-                    }
-                }
-
-                if (offsetX > offsetY+2 && offsetX > offsetZ+2) {
-                    if (entity.getPos().getX() > pos.getX()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.WEST)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.WEST);
-                    }
-                    if (entity.getPos().getX() < pos.getX()) {
-                        if (GravityChangerAPI.getGravityDirection( entity) != Direction.EAST)
-                            ((EntityAttachments) entity).setGravityTimer(5);
-                        GravityChangerAPI.setGravityDirection( entity, Direction.EAST);
-                    }
-                }
             }
-            }
-
         }
+    }
 
-        //for (Entity entity : listb) {
-        //    if(entity instanceof PlayerEntity) {
-        //        GravityChangerAPI.setGravityDirection( entity, GravityChangerAPI.getDefaultGravityDirection(entity));
-        //    }
-        //}
+    @Override
+    public void writeNbt(NbtCompound tag) {
+        super.writeNbt(tag);
+        tag.putInt("radius", radius);
+    }
 
+    @Override
+    public void readNbt(NbtCompound tag) {
+        super.readNbt(tag);
+        if (tag.contains("radius", NbtElement.INT_TYPE)) {
+            radius = tag.getInt("radius");
+        }
     }
 }
