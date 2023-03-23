302eb57fc7120fc1cc6dca65e16b853b24d0c3ab
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-23 14:31:10 -0500

    Remove some unused methods

--- src/main/java/com/fusionflux/starminer/duck/EntityAttachments.java
+++ src/main/java/com/fusionflux/starminer/duck/EntityAttachments.java
@@ -2,18 +2,11 @@ package com.fusionflux.starminer.duck;
 
 import com.fusionflux.starminer.block.entity.AbstractStarCoreBlockEntity;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.minecraft.util.math.Vec3d;
 
 public interface EntityAttachments {
     int getPlateGravityTimer();
 
     void setPlateGravityTimer(int gravityTimer);
-
-    int getStarGravityTimer();
-
-    void setStarGravityTimer(int gravityTimer);
-
-    Vec3d getLastSSMVel();
 
     Object2IntMap<AbstractStarCoreBlockEntity> getNearbyStarCores();
 }
--- src/main/java/com/fusionflux/starminer/mixin/EntityMixin.java
+++ src/main/java/com/fusionflux/starminer/mixin/EntityMixin.java
@@ -8,23 +8,15 @@ import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin implements EntityAttachments {
-    @Unique
-    private Vec3d lastSSMVel = Vec3d.ZERO;
-
-    @Shadow
-    public abstract Vec3d getVelocity();
-
     @Shadow public World world;
 
     int gravityPlateTimer = 0;
@@ -43,8 +35,6 @@ public abstract class EntityMixin implements EntityAttachments {
             gravityStarTimer--;
         }
 
-        this.lastSSMVel = this.getVelocity();
-
         if (
             (Object)this instanceof PlayerEntity player &&
                 !SimplyStarminerConfig.creativeFlyingGravity &&
@@ -61,21 +51,6 @@ public abstract class EntityMixin implements EntityAttachments {
     @Override
     public void setPlateGravityTimer(int gravityTimer) {
         this.gravityPlateTimer = gravityTimer;
-    }
-
-    @Override
-    public int getStarGravityTimer() {
-        return this.gravityStarTimer;
-    }
-
-    @Override
-    public void setStarGravityTimer(int gravityTimer) {
-        this.gravityStarTimer = gravityTimer;
-    }
-
-    @Override
-    public Vec3d getLastSSMVel() {
-        return this.lastSSMVel;
     }
 
     @Override
